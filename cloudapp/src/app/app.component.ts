37506afd0f63f8a8f2eb2fadef53c82be6773e5d
Christoph Ladurner <christoph.ladurner@tugraz.at>
2020-09-10 09:24:36 +0200

    initial commit with barebone app structure

---
+++ cloudapp/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AppService } from './app.service';
+
+@Component({
+    selector: 'app-root',
+    template: '<router-outlet></router-outlet>'
+})
+
+export class AppComponent {
+
+    constructor(private appService: AppService) { }
+
+}
